2a162ab5c4ea20c3fd03ee8c160b1f3ad1f7926a
Doug Kintop <dkintop@hotmail.com>
2019-07-10 23:28:47 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -1,8 +1,18 @@
 # require modules here
 
-def load_library
-  # code goes here
+require 'yaml'
+def load_library(yaml_file)
+   response = {"get_meaning" => {}, "get_emoticon" => {}}
+  library = YAML.load_file(yaml_file)
+  library.each do |trans, emos|
+    response["get_meaning"][emos[1]] = trans
+    response["get_emoticon"][emos[0]] = emos[1]
+  
+  end
+  response
+  
 end
+
 
 def get_japanese_emoticon
   # code goes here
@@ -11,3 +21,4 @@ end
 def get_english_meaning
   # code goes here
 end
+
